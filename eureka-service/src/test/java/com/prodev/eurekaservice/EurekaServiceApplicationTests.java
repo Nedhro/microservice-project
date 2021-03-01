5f3a6902a7e6c8f8e0c68f1d60f6d91549c4df26
nedhro <nedhro786>
2021-03-01 12:58:05 +0600

    Eureka Service added

---
+++ eureka-service/src/test/java/com/prodev/eurekaservice/EurekaServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prodev.eurekaservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
